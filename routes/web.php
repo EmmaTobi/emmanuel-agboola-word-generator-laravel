b9f2ea068736b0ebe377246d45640b01e365abc4
mac <mac@macs-MacBook-Pro-3.local>
2022-05-30 13:06:21 +0100

    setup routing & views

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RandomWordController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/words', [RandomWordController::class, 'edit']);
+    Route::post('/words', [RandomWordController::class, 'generateFreshWords'])->name('wordsCreate');
+});
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('loginSubmit');
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
